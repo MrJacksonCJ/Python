074682ce2afd78372ac2a161e0e906765a8e056f
MRJACKSONCJ <jacok_95@hotmail.com>
2024-09-19 11:56:59 -0500

    Adivina el numero}

--- Day 11/blackjack.py
+++ Day 11/blackjack.py
@@ -19,44 +19,80 @@ def suma_pccards():
         suma_pc = calculo_suma(pc_cards)
 
 def final():
-    suma_pccards()
     suma_user = calculo_suma(user_cards)
-    suma_pc = calculo_suma(pc_cards)
+
     while suma_user < 17:
         maso_bajo = input("Te recomendamos sacar otra carta quieres sacarla? 's' o 'n':").lower()
         if maso_bajo == "s":
             user_cards.append(sacar_carta())
             suma_user = calculo_suma(user_cards)
-    print(f"Tus barajas son {user_cards} y la suma de tus cartas es {suma_user}")
-    print(f"Las barajas de la casa son {pc_cards} y la suma es {suma_pc}")
+            blackjack()
+            return
+        else:
+            break
+
+    suma_pccards()
+    suma_pc = calculo_suma(pc_cards)
+    if suma_pc > 21:
+        calculo_bust()
+        return
+
+    mostrar_cartas()
+
     if suma_pc == suma_user:
         print("Empate")
-    elif suma_pc == 21:
-        print("la casa gana")
-    elif suma_user == 21:
-        print("Tu ganas")
-    elif suma_user > 21:
-        print("Bust.La casa gana. ")
-    elif suma_pc > 21:
-        print("Tu ganas")
     elif suma_user < suma_pc:
         print("La casa gana")
     elif suma_pc < suma_user:
         print("Tu ganas")
 
+def mostrar_cartas():
+    suma_user = calculo_suma(user_cards)
+    suma_pc = calculo_suma(pc_cards)
+    print(f"            Tus barajas son {user_cards} y la suma de tus cartas es {suma_user}")
+    print(f"            Las barajas de la casa son {pc_cards} y la suma es {suma_pc}")
+
+def calculo_bust():
+    suma_user = calculo_suma(user_cards)
+    suma_pc = calculo_suma(pc_cards)
+    if suma_pc > 21:
+        print("\n")
+        mostrar_cartas()
+        print("bust. Tu ganas")
+        return
+    elif suma_user > 21:
+        print("\n")
+        mostrar_cartas()
+        print("bust. La casa gana")
+        return
 
 def blackjack():
     while True:
-        print(f"Tus cartas son {user_cards} y suma {calculo_suma(user_cards)}")
-        print(f"Las primera carta del pc es [{pc_cards[0]}] ")
+        suma_pc = calculo_suma(pc_cards)
+        suma_user = calculo_suma(user_cards)
+        if suma_pc > 21 or suma_user > 21:
+            calculo_bust()
+            break
+        elif suma_pc == 21:
+            mostrar_cartas()
+            print("la casa gana")
+            break
+        elif suma_user == 21:
+            mostrar_cartas()
+            print("Tu ganas")
+            break
+        print(f"            Tus cartas son {user_cards} y suma {calculo_suma(user_cards)}")
+        print(f"            Las primera carta del pc es [{pc_cards[0]}] ")
         otra_carta = input("Quieres otra carta escribe 's' si pasas escribe 'n': ").lower()
-
         if otra_carta == "s":
             user_cards.append(sacar_carta())
         else:
+            if suma_pc > 21 or suma_user > 21:
+                calculo_bust()
+                break
             final()
             break
-
+    return
 
 global user_cards, pc_cards,cards
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -66,7 +102,7 @@ while True:
     print(art.logo)
     repartir_carta()
     blackjack()
-    de_nuevo = input("Quieres jugar de nuevo? 's' o 'n'").lower()
+    de_nuevo = input("\n Quieres jugar de nuevo? 's' o 'n'").lower()
     if de_nuevo == "s":
         print("\n" *20)
     else:
---
+++ Day 12/Guess_a_number.py
@@ -0,0 +1,35 @@
+import random
+import art
+
+def level_escoger(dificulty):
+        if dificulty == "facil":
+            return 10
+        else:
+            return 5
+
+def evaluar_numero(num_escogido):
+
+    if num_escogido < numero:
+        print("Muy bajo")
+    elif numero_escogido > numero:
+        print("Muy alto")
+    return -1
+
+print(art.logo)
+global numero
+
+numero = random.randint(1,100)
+print("Estoy pensando un numero entre 1 y 100.")
+dificultad = input("Escoje una dificultad: Escribe 'facil' o 'dificil': ").lower()
+intentos = level_escoger(dificultad)
+while intentos != 0:
+    print(f"Tienes {intentos} intentos para adivinarlo")
+    numero_escogido = int(input("Escribe un numero: "))
+    intentos += evaluar_numero(numero_escogido)
+    if numero_escogido == numero:
+        print(f"Lo adivinaste! El numero era {numero_escogido}.")
+        break
+    else:
+        if intentos >=1:
+          print("Intenta de nuevo")
+print("Ya no tienes mas intentos")
